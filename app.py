8e39ab9f6afceba839913deeec2248e06677f60a
alvinmrrry <alvin_mrrry@outlook.com>
2024-06-15 13:10:16 +0000

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from langchain_core.messages import SystemMessage
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
-from langchain_community.utilities.tavily_search import TavilySearchResults
+from langchain_community.tools.tavily_search.tool import TavilySearchResults
 
 
 groq_api_key = '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'
